1b4a733e2fe5899bcefa7619c6a99e34fa40e881
Seth Babcock <k652gzv77y@privaterelay.appleid.com>
2026-09-03 23:28:47 -0600

    Add production wrangler.jsonc and OpenNext config for Workers Builds

---
+++ open-next.config.ts
@@ -0,0 +1,8 @@
+import { defineCloudflareConfig } from '@opennextjs/cloudflare';
+import kvIncrementalCache from '@opennextjs/cloudflare/overrides/incremental-cache/kv-incremental-cache';
+import doShardedTagCache from '@opennextjs/cloudflare/overrides/tag-cache/do-sharded-tag-cache';
+
+export default defineCloudflareConfig({
+  incrementalCache: kvIncrementalCache,
+  tagCache: doShardedTagCache({ baseShardSize: 4 }),
+});
